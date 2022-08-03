93a165588cd052da616ccab5eb51ea066a953ac4
서벨롭 <pypygeek@gmail.com>
2022-08-03 10:11:33 +0000

    Add Wirte Page

--- costory/posts/models.py
+++ costory/posts/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, unique=True, error_messages={'unique':'이미 있는 제목이네요!'})
     content = models.TextField()
-    dt_created = models.DateTimeField(verbose_name="Date Created", auto_now_add=True)
-    dt_modified = models.DateTimeField(verbose_name="Date Modified", auto_now=True)
+    dt_created = models.DateField(verbose_name='Date Created', auto_now_add=True)
+    dt_modified = models.DateTimeField(verbose_name="Date Modified", auto_now = True)
 
     def __str__(self):
         return self.title
--- costory/posts/views.py
+++ costory/posts/views.py
@@ -1,14 +1,24 @@
-from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
-def post_list(request):
+def post_list(requset):
     posts = Post.objects.all()
-    context = {"posts" : posts}
-    return render(request, 'posts/post_list.html', context)
+    context = {'posts': posts}
+
+    return render(requset, 'posts/post_list.html', context)
 
 def post_detail(request, post_id):
     post = Post.objects.get(id=post_id)
-    context = {"post" : post}
-    return render(request, 'posts/post_detail.html', context)
+    return render(request, 'posts/post_detail.html', {'post':post})
+
+def post_create(request):
+    if request.method == 'POST':
+        post_form = PostForm(request.POST)
+        if post_form.is_valid:
+            new_post = post_form.save()
+            return redirect('post-detail', post_id=new_post.id)
+    else:
+        post_form = PostForm
+        return render(request, 'posts/post_form.html', {'form': post_form})
