9e02bde5ebb99d63dc1e15316934a44b99e4adf3
서벨롭 <pypygeek@gmail.com>
2022-08-03 05:39:29 +0000

    Django ORM Study

--- costaurant/costaurant/settings.py
+++ costaurant/costaurant/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b)280q5&esyz$5o5dvzfkht(nsa#b-ig=*kaw6cblq@@jy(b(@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# python manage.py collectstatic
