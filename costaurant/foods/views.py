e8e28d7b770f59941df28623c6ccf00ae3588966
서벨롭 <pypygeek@gmail.com>
2022-08-03 02:13:52 +0000

    Add file

---
+++ costaurant/foods/views.py
@@ -0,0 +1,22 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from django.http import Http404
+
+# Create your views here.
+def index(request):
+    today = datetime.today().date()
+    context = {"date":today}
+    return render(request, 'foods/index.html', context=context)
+
+def food_detail(request, food):
+    context = dict()
+    if food == "chicken":
+        context["name"] = "코딩에 빠진 닭"
+        context["description"] = "주머니가 가벼운 당신의 마음까지 생각한 가격!"
+        context["price"] = 1000
+        context["img_path"] = "foods/images/chicken.jpg"
+    else:
+        raise Http404("이런 음식은 없습니다!")
+    return render(request, 'foods/detail.html', context=context)
