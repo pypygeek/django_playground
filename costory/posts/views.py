312deca4c086ab00e248c1bb83820d1fed26286e
서벨롭 <pypygeek@gmail.com>
2022-08-08 03:18:18 +0000

    Add No data show 404 Error

--- costory/posts/views.py
+++ costory/posts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404
 from .models import Post
 from .forms import PostForm
 
@@ -10,7 +11,8 @@ def post_list(requset):
     return render(requset, 'posts/post_list.html', context)
 
 def post_detail(request, post_id):
-    post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, id=post_id)
+        
     return render(request, 'posts/post_detail.html', {'post':post})
 
 def post_create(request):
@@ -24,7 +26,7 @@ def post_create(request):
     return render(request, 'posts/post_form.html', {'form': post_form})
 
 def post_update(request, post_id):
-    post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, id=post_id)
     if request.method == 'POST':
         post_form = PostForm(request.POST, instance=post)
         if post_form.is_valid():
@@ -35,7 +37,7 @@ def post_update(request, post_id):
     return render(request, 'posts/post_form.html', {'form':post_form})
 
 def post_delete(request, post_id):
-    post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, id=post_id)
     if request.method == 'POST':
         post.delete()
         return redirect('post-list')
