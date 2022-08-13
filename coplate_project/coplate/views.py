93c8e76427e5fabc5227fdbfac157574bc87853c
서벨롭 <pypygeek@gmail.com>
2022-08-13 15:31:59 +0000

    review edit

--- coplate_project/coplate/views.py
+++ coplate_project/coplate/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from allauth.account.views import PasswordChangeView
 from coplate.models import Review
+from coplate.forms import ReviewForm
 
 # Create your views here.
 class IndexView(ListView):
@@ -16,6 +23,35 @@ class ReviewDetailView(DetailView):
     model = Review
     template_name = "coplate/review_detail.html"
     pk_url_kwarg = "review_id"
+
+class ReviewCreateView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "coplate/review_form.html"
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse("review-detail", kwargs={"review_id":self.object.id})
+    
+class ReviewUpdateView(UpdateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "coplate/review_form.html"
+    pk_url_kwarg = "review_id"
+    
+    def get_success_url(self):
+        return reverse("review-detail", kwargs={"review_id":self.object.id})
+
+class ReviewDeleteView(DeleteView):
+    model = Review
+    template_name = "coplate/review_confirm_delete.html"
+    pk_url_kwarg = "review_id"
+    
+    def get_success_url(self):
+        return reverse("index")
 
 class CustomPasswordChangeView(PasswordChangeView):
     def get_success_url(self):
