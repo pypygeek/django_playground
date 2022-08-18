cb52d28af253ea56477f7fe6fb80faba10a5f3c9
서벨롭 <pypygeek@gmail.com>
2022-08-18 02:15:49 +0000

    upload models

--- doit_blog/blog/views.py
+++ doit_blog/blog/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
