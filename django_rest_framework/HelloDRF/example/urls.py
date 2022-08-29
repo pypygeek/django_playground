5a8068592a341e8122e1033eef287a1742922272
서벨롭 <pypygeek@gmail.com>
2022-08-29 09:45:01 +0000

    End HelloDRF

--- django_rest_framework/HelloDRF/example/urls.py
+++ django_rest_framework/HelloDRF/example/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
-from .views import HelloAPI, BooksAPI, BookAPI
+from .views import helloAPI, bookAPI, booksAPI, BookAPI, BooksAPI, BooksAPIMixins, BookAPIMixins
 
 urlpatterns = [
-    path('hello/', HelloAPI.as_view()),
-    path('books/', BooksAPI.as_view()),
-    path('book/<int:bid>/', BookAPI.as_view()),
+    path("hello/", helloAPI),
+    path("fbv/books/", booksAPI),
+    path("fbv/book/<int:bid>/", bookAPI),
+    path("cbv/books/", BooksAPI.as_view()),
+    path("cbv/book/<int:bid>/", BookAPI.as_view()),
+    path("mixin/books/", BooksAPIMixins.as_view()),
+    path("mixin/book/<int:bid>/", BookAPIMixins.as_view()),
 ]
